e2c9ef098cb0c2690806e818aab7cb4451381a65
Krishu7827 <krishukumar7827@gmail.com>
2024-03-14 18:57:41 +0530

    Registration

--- Controller/Person.Controller.js
+++ Controller/Person.Controller.js
@@ -1,5 +1,7 @@
+const {dbConn} = require('../db.config/db.config')
+
 const PersonRegister = (req, res) => {
     const { personid, employeeid, loginid, joblocation, fullname, department, designation } = req.body
-
-    
+     
+    const query = `CALL PersonRegister('${personid}','${employeeid}','${loginid}', '${joblocation}', '${fullname}')`
 }
