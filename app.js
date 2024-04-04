2941cbcb094ad8e07d7fb09e200bc6d23b155517
Krishna Kumar <117253066+Krishu7827@users.noreply.github.com>
2024-04-04 10:38:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use('/IQCSolarCell',IQCSolarCellRoute)
 app.use('/IPQC',IPQCJobCardRouter);
 
 app.get('/',()=>{
-  res.send(Hello In QCM Backend);
+  res.send('Hello In QCM Backend');
 })
 
 app.listen(PORT,async()=>{
