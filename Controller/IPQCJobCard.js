e0c338e3a69402e67417a2b81f59ef2a82130f0b
Krishna Kumar <117253066+Krishu7827@users.noreply.github.com>
2024-04-20 17:35:15 +0530

    Update IPQCJobCard.js

--- Controller/IPQCJobCard.js
+++ Controller/IPQCJobCard.js
@@ -150,7 +150,8 @@ const AddIPQCJobCard = async (req, res) => {
         jcd.Date = '${JobCardDetails['date']}',
         jcd.MatrixSize = '${JobCardDetails['matrixSize']}',
         jcd.Status = '${JobCardDetails['Status']}',
-        jcd.CreatedBy = '${JobCardDetails['CreatedBy']}'
+        jcd.CreatedBy = '${JobCardDetails['CreatedBy']}',
+        jcd.CreatedOn = '${getCurrentDateTime()}'
     WHERE jcd.JobCardDetailID = '${JobCardDetailId}';`
 
       await queryAsync(QueryToJobCardDetails)
@@ -203,7 +204,7 @@ JOIN BOMVerificationDetails bd ON p.PersonID = bd.CheckedBy
 WHERE bd.Status = '${Status}'
 ORDER BY STR_TO_DATE(bd.CreatedOn, '%d-%m-%Y %H:%i:%s') DESC;`;
 
-      PreLamQuery = `SELECT p.EmployeeID,  p.Name, p.ProfileImg, wl.Location,PD.PreLamDetailId,PD.PONo,PD.Type,PD.PreLamPdf, PD.CreatedOn FROM Person p
+      PreLamQuery = `SELECT p.EmployeeID,  p.Name, p.ProfileImg, wl.Location,PD.PreLamDetailId,PD.PONo,PD.Line,PD.Type,PD.PreLamPdf, PD.CreatedOn FROM Person p
 JOIN WorkLocation wl ON wl.LocationID = p.WorkLocation
 JOIN PreLamDetail PD ON p.PersonID = PD.CheckedBy
 WHERE PD.Status = '${Status}'
@@ -224,7 +225,7 @@ JOIN BOMVerificationDetails bd ON p.PersonID = bd.CheckedBy
 WHERE bd.Status = '${Status}' AND p.PersonID = '${PersonID}'
 ORDER BY STR_TO_DATE(bd.CreatedOn, '%d-%m-%Y %H:%i:%s') DESC;`;
 
-      PreLamQuery = `SELECT p.EmployeeID,  p.Name, p.ProfileImg, wl.Location,PD.PreLamDetailId,PD.PONo,PD.Type,PD.PreLamPdf, PD.CreatedOn FROM Person p
+      PreLamQuery = `SELECT p.EmployeeID,  p.Name, p.ProfileImg, wl.Location,PD.PreLamDetailId,PD.PONo,PD.Line,PD.Type,PD.PreLamPdf, PD.CreatedOn FROM Person p
 JOIN WorkLocation wl ON wl.LocationID = p.WorkLocation
 JOIN PreLamDetail PD ON p.PersonID = PD.CheckedBy
 WHERE PD.Status = '${Status}' AND p.PersonID = '${PersonID}'
@@ -250,6 +251,9 @@ ORDER BY STR_TO_DATE(PD.CreatedOn, '%d-%m-%Y %H:%i:%s') DESC;`;
 
     PreLamList.forEach((BOM) => {
       for (let key in BOM) {
+        
+//PreLam PostLam
+        if(BOM['Type'] == 'PreLam' || BOM['Type'] == 'PostLam'){
         if (key == 'PreLamDetailId') {
           BOM['JobCardDetailID'] = BOM[key]
           delete BOM[key]
@@ -260,8 +264,21 @@ ORDER BY STR_TO_DATE(PD.CreatedOn, '%d-%m-%Y %H:%i:%s') DESC;`;
           BOM['ReferencePdf'] = BOM[key]
           delete BOM[key]
         }
+        delete BOM['Line'];
+      }else if(BOM['Type'] == 'Framing Dimension'){
+        if (key == 'PreLamDetailId') {
+          BOM['JobCardDetailID'] = BOM[key]
+          delete BOM[key]
+        } else if (key == 'Line') {
+          BOM['ModuleNo'] = BOM[key]
+          delete BOM[key]
+        }else if(key == 'PreLamPdf'){
+          BOM['ReferencePdf'] = BOM[key]
+          delete BOM[key]
+        }
       }
-      JobCardList.push(BOM)
+      }
+      JobCardList.push(BOM);
     })
  /** Function to parse the date string into a Date object for comparison **/
 const parseDate = dateString => {
