fcea5d4bf2ca046f876576a25734973266e2e71c
Krishna Kumar <117253066+Krishu7827@users.noreply.github.com>
2024-04-23 18:21:15 +0530

    Update IQCSolarCellRoute.js

--- Routes/IQCSolarCellRoute.js
+++ Routes/IQCSolarCellRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const IQCSolarCellRoute = express.Router()
-const {AddIQCSolarCell, GetIQCSolarCellTests,GetSpecificSolarCellTest,UpdateStatus,UploadPdf} = require('../Controller/IQCSolarCell')
+const {AddIQCSolarCell, GetIQCSolarCellTests,GetSpecificSolarCellTest,UpdateStatus,UploadPdf} = require('../Controller/IQCSolarCell');
+const {AddFQC,GetFQCList,GetSpecificFQC,FQCUpdateStatus,UploadFQCPdf} = require('../Controller/IQCFQC.Controller')
 const {RoleAuthentication,upload} = require('../Middleware/IQCSolarCell.Middleware')
-
+const {FQCUpload} = require('../Middleware/FQC.Middleware')
 
 /** to add IQC Solar Cell  */
 IQCSolarCellRoute.post('/AddIQCSolarCell',AddIQCSolarCell)
@@ -10,6 +11,11 @@ IQCSolarCellRoute.post('/AddIQCSolarCell',AddIQCSolarCell)
 /**to Upload PDF */
 IQCSolarCellRoute.post('/UploadPdf',upload,UploadPdf)
 
+/**to Upload PDF */
+IQCSolarCellRoute.post('/UploadFQCPdf',FQCUpload.single('FQCPdf'),UploadFQCPdf)
+
+/**to Add FQC Data in FQCDetails Table And FQCTest Table */
+IQCSolarCellRoute.post('/AddFQC',AddFQC);
 
 /** Middleware To Role Authentication  */
 IQCSolarCellRoute.use(RoleAuthentication)
@@ -23,7 +29,14 @@ IQCSolarCellRoute.post('/GetSpecificTest',GetSpecificSolarCellTest)
 /** to Update Status of Solar Cell Details Table and Creating new Column in ApprovalStatus table*/
 IQCSolarCellRoute.post('/UpdateStatus',UpdateStatus)
 
+/**to Get FQC Card List */
+IQCSolarCellRoute.post('/FQCList',GetFQCList)
 
+/**to Get Specific FQC Detail */
+IQCSolarCellRoute.post('/GetSpecificFQC',GetSpecificFQC)
+
+/**To Update Status of Aprrove in FQC Table */
+IQCSolarCellRoute.post('/FQCUpdateStatus',FQCUpdateStatus)
 
 /** Exporting Routes */
 module.exports = {IQCSolarCellRoute}
