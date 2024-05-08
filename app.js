73f0922ac5e74e0b7dbcf590f3d64c58325df53d
Krishna Kumar <117253066+Krishu7827@users.noreply.github.com>
2024-05-08 15:51:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const {dbConn} = require('./db.config/db.config')
-const {PersonRouter} = require('./Routes/Person.Route')
-const {designationRouter} = require('./Routes/DesignationRoute')
-const {IQCSolarCellRoute} = require('./Routes/IQCSolarCellRoute')
-const {IPQC} = require('./Routes/IPQCRoute')
+const { dbConn } = require('./db.config/db.config')
+const { PersonRouter } = require('./Routes/Person.Route')
+const { designationRouter } = require('./Routes/DesignationRoute')
+const { IQCSolarCellRoute } = require('./Routes/IQCSolarCellRoute')
+const { QualityRoute } = require('./Routes/QualityRoutes')
+const path = require('path')
+const { IPQC } = require('./Routes/IPQCRoute')
 const app = express()
 const cors = require('cors')
 const PORT = process.env.PORT || 8080
@@ -32,6 +34,8 @@ app.use('/IQCSolarCell',IQCSolarCellRoute)
 /**to IPQC */
 app.use('/IPQC',IPQC);
 
+/**to Quality */
+app.use('/Quality', QualityRoute)
 
 
 app.listen(PORT,async()=>{
