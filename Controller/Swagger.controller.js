74992c6fef43ce533898285b3904d791affd0147
Krishna Kumar <117253066+Krishu7827@users.noreply.github.com>
2024-07-29 17:26:49 +0530

    Create Swagger.controller.js

---
+++ Controller/Swagger.controller.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const Port = process.env.PORT || 9090
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Machine Maintenace API Documentation',
+      version: '1.0.0',
+      description: 'API Documentation',
+    },
+    servers: [
+      {
+        url: `http://srv515471.hstgr.cloud:${Port}`, // Change this to your server URL
+      },
+    ],
+  },
+  apis: ['./Routes/*.js'], // Path to the API docs
+};
+
+const swaggerSpec = swaggerJsDoc(options);
+
+const setupSwagger = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
+
+module.exports = {setupSwagger};
